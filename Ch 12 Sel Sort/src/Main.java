19187469d4d5c0e9608192a6793902ec864b5cd7
brandwarbs <bwarbeltron@gmail.com>
2013-02-20 11:00:45 -0600

    stuff

--- Ch 12 Sel Sort/src/Main.java
+++ Ch 12 Sel Sort/src/Main.java
@@ -4,28 +4,57 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		double[] array1 = {5.0, 4.4, 1.9, 2.9, 3.4, 3.5};
-
+	public static void main(String[] args) 
+	{
+		double[] array1 = {1.0, 4.4, 1.9, 2.9, 3.4, 3.5};
+		double tempInd;
+		
+		//selectionSort(array1);
+		tempNum = findMaximum(array1, array1.length - 1);
+		System.out.println(tempNum);
+		printArray(array1.length, array1);
+		
 	}
 	
-	void selectionSort(double[] a)
+	public static void selectionSort(double[] a)
 	{
-		for (int i = 0; i < a.length - 1; i++)
+		for (int i = a.length; i < 0 - 1; i--)
 		{
-			int minIndex = findMinimum(a, i);
-			if (minIndex != i)
-				swap(a, i, minIndex);
+			int maxIndex = findMaximum(a, i);
+			if (maxIndex == i)
+				swap(a, i, maxIndex);
 		}
 	}
 
-	void swap(double[] a, int x, int y) {
-		// TODO Auto-generated method stub
-		
+	public static void swap(double[] a, int x, int y) 
+	{
+		double temp = a[x];
+		a[x] = a[y];
+		a[y] = temp;
 	}
 
-	int findMinimum(double[] a, int first) {
-		// TODO Auto-generated method stub
-		return 0;
+	public static int findMaximum(double[] a, int first) 
+	{
+		int maxIndex = first;
+		for (int i = first + 1; i < a.length ; i++)
+		{
+			if (a[i] > a[maxIndex])
+				maxIndex = i;
+		}
+		
+		return maxIndex;
+	}
+	public static void printArray(int arrayLength, double[] array)
+	{
+		String str = "";
+		for (int i = 0; i < arrayLength; i++)
+		{
+			if (i != arrayLength - 1)
+				str += array[i] + ", ";
+			else
+				str += array[i]; 
+		}
+
+		System.out.println(str);
 	}
 }
