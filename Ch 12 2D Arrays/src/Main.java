88ac8efc13f46165dde338cbddbc05943ccb5f38
brandwarbs <bwarbeltron@gmail.com>
2013-03-04 12:34:53 -0600

    submit

--- Ch 12 2D Arrays/src/Main.java
+++ Ch 12 2D Arrays/src/Main.java
@@ -6,7 +6,10 @@ public class Main {
 	 */
 	public static void main(String[] args) 
 	{
-		
+		int[][] m1 = {{2, 3, -1}, {5, -2, 2}};
+		int[][] m2 = {{5, 3}, {1, -1}, {-2, 0}};
+		int[][] m3 = {{1, 2}, {3, 4}};
+		int[][] m4 = {{-2, 4}, {7, 3}};
 	}
 
 }
