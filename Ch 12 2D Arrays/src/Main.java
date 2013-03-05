8e2aa58becba7fa27c10ac2a03eec686687a60e5
brandwarbs <bwarbeltron@gmail.com>
2013-03-05 10:59:45 -0600

    some

--- Ch 12 2D Arrays/src/Main.java
+++ Ch 12 2D Arrays/src/Main.java
@@ -6,10 +6,19 @@ public class Main {
 	 */
 	public static void main(String[] args) 
 	{
-		int[][] m1 = {{2, 3, -1}, {5, -2, 2}};
-		int[][] m2 = {{5, 3}, {1, -1}, {-2, 0}};
-		int[][] m3 = {{1, 2}, {3, 4}};
-		int[][] m4 = {{-2, 4}, {7, 3}};
+		int[][] m1 = {{2, 3, -1},
+					{5, -2, 2}};
+		int[][] m2 = {{5, 3}, 
+					{1, -1}, 
+					{-2, 0}};
+		int[][] m3 = {{1, 2},
+					{3, 4}};
+		int[][] m4 = {{-2, 4},
+					{7, 3}};
 	}
 
+	public static int[][] addMatrix(int[][] matrix1, int[][] matrix2)
+	{
+		return null;
+	}
 }
