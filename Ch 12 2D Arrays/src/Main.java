3d1dcc7a1f5a4457bdede36be512dc5bfe025806
brandwarbs <bwarbeltron@gmail.com>
2013-03-06 10:54:57 -0600

    sort of does addition

--- Ch 12 2D Arrays/src/Main.java
+++ Ch 12 2D Arrays/src/Main.java
@@ -15,11 +15,30 @@ public class Main {
 					{3, 4}};
 		int[][] m4 = {{-2, 4},
 					{7, 3}};
+		
+		System.out.println(canAdd(m3, m4));
+		System.out.println(canAdd(m1, m2));
+		System.out.println(canAdd(m3, m2));
+		System.out.println(canAdd(m1, m4));
+		System.out.println(stringMatrix(addMatrix(m3, m4)));
+		//System.out.println(stringMatrix(addMatrix(m2, m4)));
 	}
 
 	public static int[][] addMatrix(int[][] matrix1, int[][] matrix2)
 	{
-		
+		if(canAdd(matrix1, matrix2))
+		{
+			int[][] theMatrix = new int[matrix1.length][matrix1.length];
+			for(int i = 0; i < matrix1.length; i++)
+			{
+				for(int u = 0; u < matrix1[i].length; u++)
+				{
+					theMatrix[i][u] = matrix1[i][u] + matrix2[i][u];
+				}
+			}
+			
+			return theMatrix;
+		}
 		
 		return null;
 	}
@@ -27,10 +46,38 @@ public class Main {
 	public static boolean canAdd(int[][] trix1, int[][] trix2)
 	{
 		boolean size = true;
+		int firstSize = trix1.length;
+		if(firstSize != trix2.length)
+			size = false;
 		
-		for(int i = 0)
+		for(int i = 0; i < trix1.length; i++)
+		{
+			if(firstSize != trix1[i].length)
+				size = false;
+		}
+		for(int i = 0; i < trix2.length; i++)
+		{
+			if(firstSize != trix2[i].length)
+				size = false;
+		}
 		
 		return size;
+	}
+	
+	public static String stringMatrix(int[][] mat)
+	{
+		String str = "";
+		
+		for(int i = 0; i < mat.length; i++)
+		{
+			for(int u = 0; u < mat[i].length; u++)
+			{
+				str += mat[i][u] + " "; 
+			}
+			str += "\n";
+		}
+		
+		return str;
 	}
 	
 }
