b7c871ec30380cdabca4053cec49bba415017765
brandwarbs <bwarbeltron@gmail.com>
2013-03-13 12:33:49 -0500

    stuff

--- Ch 14 Arraylists/src/Main.java
+++ Ch 14 Arraylists/src/Main.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
+
 
 public class Main {
 
 	/**
 	 * @author Brandon Warbelton
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		ArrayList<String> sentenceList = new ArrayList<String>();
+		String sentence = "The big black bear and the big black dog ran down the road";
 		
-
 	}
 
+	public static void printArrayList(ArrayList<String> list)
+	{
+		
+	}
+	
+	
 }
